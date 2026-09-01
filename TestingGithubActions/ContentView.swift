ba5a1085dd3507ebd5c50469500e4e31ea81ee6d
Shagraf6 <shagrafnasheet@gmail.com>
2026-09-01 19:59:08 +0500

    first commit

---
+++ TestingGithubActions/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  TestingGithubActions
+//
+//  Created by MACBOOK on 31/08/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+        .onAppear{
+           
+        }
+    }
+        
+    
+    
+}
+
+#Preview {
+    ContentView()
+}
+
+}
---
+++ TestingGithubActions/TestingGithubActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingGithubActionsApp.swift
+//  TestingGithubActions
+//
+//  Created by MACBOOK on 31/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct TestingGithubActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
